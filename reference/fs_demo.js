403fdbe02e78fb81dace12ed9c837cf484ae26b7
Michel Frechette <mifrec@hotmail.com>
2020-01-13 10:53:58 -0500

    File System module fs.write.File Create and Write to File

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -7,3 +7,14 @@ fs.mkdir(path.join(__dirname, '/test'), {}, err => {
   if (err) throw err;
   console.log('Folder created...');
 });
+
+// Create and write to file
+
+fs.writeFile(
+  path.join(__dirname, '/test', 'hello.txt'),
+  'Hello World!',
+  err => {
+    if (err) throw err;
+    console.log('File written to...');
+  }
+);
